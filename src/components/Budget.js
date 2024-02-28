09e426ad82e58989029f34e6fa362e66a2d859ab
rachidbll <rachidbll72@gmail.com>
2024-02-28 07:14:05 -0500

    still dealing with the expense limit

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,29 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import ExpenseTotal from './ExpenseTotal';
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
 
+    const handleBudgetChange = (event) => {
+        const updatedBudget = event.target.value;
+        setNewBudget(updatedBudget);
+
+        // Check if the new budget exceeds 20000
+        if (updatedBudget > 20000) {
+            alert("Exceeded the budget limit of £20000!");
+            // Reset the input value to the current budget to prevent exceeding the limit
+            setNewBudget(budget);
+            
+        }
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
+    );
+};
+
+export default Budget;
